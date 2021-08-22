35e8a824abd0de0b18b1432f3ee60ebf5d6097a2
Serge Sroujian <ssroujian@outlook.com>
2021-08-21 23:26:40 -0400

    First Commit
    This is the first commit containing React app boiler plate + NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
+
+function NavBar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => {
+    setClick(!click);
+  };
+
+  const closeMobileMenu = () => {
+    setClick(false);
+  };
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <div>
+      <nav className='navbar'>
+        <div className='nav-bar-container'>
+          <Link to='/' className='navbar-logo'>
+            Hello
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+          </div>
+          <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                Products
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/signup' className='nav-links' onClick={closeMobileMenu}>
+                Sign-Up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>Sign UP</Button>}
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
